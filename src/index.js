5466a08ec6452b4fb4b72c91e8e08fe1b68df4af
jrlimueno@gmail.com <jrlimueno@gmail.lcom>
2022-11-12 01:48:07 +0200

    inline style, scss, dynamic style,class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import './index.scss';
 import App from './components/app/app'  /* заходим в папку */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
